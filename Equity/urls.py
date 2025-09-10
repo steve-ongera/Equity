2030286ad3c3e9ed22d93a829a0af971e07f2f0f
steve <gadafisteve001@gmail.com>
2025-09-10 12:23:47 +0300

    added views templates

--- Equity/urls.py
+++ Equity/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for Equity project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import handler400, handler403, handler404, handler500
+from banking_system import views as banking_views  # import your app views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('banking_system.urls')),  # include your app urls
 ]
+
+# Serve static & media in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom error handlers
+handler400 = 'banking_system.views.custom_400'
+handler403 = 'banking_system.views.custom_403'
+handler404 = 'banking_system.views.custom_404'
+handler500 = 'banking_system.views.custom_500'
--- banking_system/views.py
+++ banking_system/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def custom_400(request, exception=None):
+    return render(request, "errors/400.html", status=400)
+
+def custom_403(request, exception=None):
+    return render(request, "errors/403.html", status=403)
+
+def custom_404(request, exception=None):
+    return render(request, "errors/404.html", status=404)
+
+def custom_500(request):
+    return render(request, "errors/500.html", status=500)
